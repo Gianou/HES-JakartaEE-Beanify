600f25416201b3c57602d0629c65feb624e8d018
Gianou <david.gianadda@students.hevs.ch>
2023-04-12 16:21:59 +0200

    relations added to entities

--- src/main/java/ch/hevs/businessobject/Artist.java
+++ src/main/java/ch/hevs/businessobject/Artist.java
@@ -1,12 +1,17 @@
 package ch.hevs.businessobject;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -15,7 +20,15 @@ public class Artist extends Person {
 
 	@Column(name="artistName")
 	private String artistName;
+	
+	@OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
+	private Set<Album> albums;
 
+	@ManyToMany
+	private Set<Song> songs;
+	
+	
+	// GETTER SETTER
 	public String getArtistName() {
 		return artistName;
 	}
@@ -23,14 +36,35 @@ public class Artist extends Person {
 	public void setArtistName(String artistName) {
 		this.artistName = artistName;
 	}
+
+	public Set<Album> getAlbums() {
+		return albums;
+	}
+
+	public void setAlbums(Set<Album> albums) {
+		this.albums = albums;
+	}
+
+	public Set<Song> getSongs() {
+		return songs;
+	}
+
+	public void setSongs(Set<Song> songs) {
+		this.songs = songs;
+	}
 	
+	// CONSTRUCTOR
 	public Artist(long id, String firstName, String lastName) {
 		super(id, firstName, lastName);
-		// TODO Auto-generated constructor stub
+		albums = new HashSet<Album>();
+		songs = new HashSet<Song>();
 	}
 	
 	public Artist()
 	{
 		super();
+		albums = new HashSet<Album>();
+		songs = new HashSet<Song>();
 	}
+	
 }
